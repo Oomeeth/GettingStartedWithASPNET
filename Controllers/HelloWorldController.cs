13b914f23659636be01b40b6f622e09c249f63d2
Cr8v Dev <cr8vdev@gmail.com>
2020-08-09 20:19:28 +0200

    Creating a controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Getting_Started.Controllers
+{
+
+    public class HelloWorldController : Controller
+    {
+        /*
+         * public methods are callable HTTP endpoints, which is a targetable URL.
+         * Run (CTRL + F5) the app and try appending 'HelloWorld' to the URL. Then try appending 'HelloWorld/Index' and 'HelloWorld/Welcome'
+         * Have a look at Configure in Startup.cs to view the format for the URL routing logic
+         */
+
+        public string Index()
+        {
+            return "This is my default action.";
+        }
+
+        public string Welcome()
+        {
+            return "This is the welcome action method.";
+        }
+
+        /*
+         * The method below allows you to specify paramteres to the URL.
+         * Run the app and append the following to the URL 'HelloWorld/WelcomeParams?name=John&id=4'
+         * 
+         * Another way to run this would be to append the following to the URL 'HelloWorld/Welcome/3?name=Rick'
+         * Notice that the above URL matches the routing format in Startup.cs Config() '{controller=Home}/{action=Index}/{id?}'
+         */
+
+        public string WelcomeParams(string name, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is {id}");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Getting_Started.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
