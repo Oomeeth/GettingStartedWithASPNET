55fa6066ff12c80a45bd1ff557a237a7c8ab6d2d
Cr8v Dev <cr8vdev@gmail.com>
2020-11-09 20:21:00 +0200

    Tests

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Getting_Started.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, string surname, string id)
+        {
+            ViewData["method"] = "Welcome";
+            ViewData["name"] = name;
+            ViewData["surnname"] = surname;
+            ViewData["id"] = id;
+            ViewData["repeat"] = 3;
+
+            return View();
+        }
+    }
+}
